2856256911efe47a274fe74ef0db8590c7492b8f
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-10-06 10:44:24 +0530

    Sort games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,8 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) => {
   return useData<Game>("/games",{params: { genres: gameQuery.genre?.id , 
-    platforms: gameQuery.platform?.id }}, [gameQuery]);
+    platforms: gameQuery.platform?.id , ordering: gameQuery.sortOrder},
+ }, [gameQuery]);
 };
 
 export default useGames;
