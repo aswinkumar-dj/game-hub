a36091f9d1058dc634c47cbe0a442af82d5f16a7
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-10-06 09:32:57 +0530

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,9 +2,12 @@ import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkeleton from "./GameCardSkeleton";
-
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+import type { Genre } from "../hooks/useGenres";
+interface Props {
+  selectedGenre: Genre | null;
+}
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
