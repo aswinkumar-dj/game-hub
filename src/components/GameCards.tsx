73529df08fb88f484043a6e8c12cd75c2c5867c1
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2026-02-03 16:59:49 +0530

    Fetching game details in Details page

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -3,13 +3,19 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
 }
 const GameCards = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card
+      borderRadius={10}
+      overflow="hidden"
+      _hover={{ transform: "scale(1.03)" }}
+      transition="transform 0.2s ease-in-out"
+    >
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
@@ -18,7 +24,9 @@ const GameCards = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
+        </Heading>
       </CardBody>
     </Card>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
+
 import theme from "./theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./route";
 
 const rootElement = document.getElementById("root");
 const queryClient = new QueryClient();
@@ -14,10 +16,10 @@ if (rootElement) {
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <QueryClientProvider client={queryClient}>
-          <App />
+          <RouterProvider router={router} />
           <ReactQueryDevtools />
         </QueryClientProvider>
       </ChakraProvider>
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 }
